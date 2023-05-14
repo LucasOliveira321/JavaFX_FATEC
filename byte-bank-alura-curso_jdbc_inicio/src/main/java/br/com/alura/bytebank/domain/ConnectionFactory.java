1eb96d84d30e2f212d6252a935a5baafc39328ff
Lucas Oliveira <107444159+LucasOliveira321@users.noreply.github.com>
2023-05-14 20:33:17 -0300

    Conteúdo de aula JDBC

---
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/domain/ConnectionFactory.java
@@ -0,0 +1,18 @@
+package br.com.alura.bytebank.domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public Connection recuperaConexao(){
+        try {
+            return DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/BDteste","postgres", "123456");
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
